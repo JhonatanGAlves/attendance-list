1b2cb16c2d5cf37e79844c648306f2bd00b09cee
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-06-16 22:15:07 -0300

    🎨 title: GlobalStyle component created

---
+++ src/styles/GlobalStyles/index.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-family: 'Source Code Pro', monospace;
+
+    background: #151F30;
+  }
+`
+
+export default GlobalStyle
