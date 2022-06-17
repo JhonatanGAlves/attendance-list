ab0c254152dc638b89cd32155e6e17ed0c61a459
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-06-16 23:05:18 -0300

    👨🏼‍💻 title: populating the Card component and passing the properties

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,8 +1,15 @@
 import { Container } from "./styles"
 
-export const Card = () => {
+type CardProps = {
+  name: string,
+  time: string
+}
+
+export const Card = ({ name, time }: CardProps) => {
   return (
     <Container>
+      <p>{name}</p>
+      <small>{time}</small>
     </Container>
   )
 }
