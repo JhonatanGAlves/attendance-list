338c7306c458d34725da90b59844d7726647e03d
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-06-16 22:19:49 -0300

    ➕ title: import Home component to App component

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import GlobalStyle from "../styles/GlobalStyles"
+import { Home } from "./Home"
+
+export const App = () => {
+  return (
+    <>
+      <Home />
+      <GlobalStyle />
+    </>
+  )
+}
