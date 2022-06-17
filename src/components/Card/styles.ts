ec829094f58f58a68e39c64ec5274faabee49ad2
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-06-16 23:05:48 -0300

    🎨 title: add styles to Card component

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,5 +1,27 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 900px;
+  max-width: 900px;
+  min-width: 200px;
+  height: 100px;
+  padding: 0 15px;
+  margin-bottom: 20px;
+
+  border-radius: 10px;
+
+  color: #FFFFFF;
+  background: #0593A2;
+
+  p, small {
+    font-size: 20px;
+  }
+
+  p {
+    font-weight: 700;
+  }
 `
